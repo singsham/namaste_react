c4e68b129e28e2d16df0f70853a261ff5f4e2ab1
singsham <shambu_singh@outlook.com>
2023-11-15 23:18:06 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,12 @@
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log("🚀 ~ file: App.js:2 ~ root:", root)
+
+
+const header = React.createElement("div",{id:'parent'},
+    React.createElement('div',{id:'children'},
+    [React.createElement('div',{},'this is a child1'),React.createElement('div',{``},'this is a child2')]
+    ));
+
+    console.log("🚀 ~ file: App.js:4 ~ header:", header)
+        
+root.render(header);
