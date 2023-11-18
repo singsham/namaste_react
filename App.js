852dcbcce8a276715d68313c3034cdb14e9c99b2
singsham <shambu_singh@outlook.com>
2023-11-18 08:24:58 +0530

    create element using react

--- App.js
+++ App.js
@@ -2,14 +2,11 @@ import React  from "react";
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log("🚀 ~ file: App.js:2 ~ root:", root)
+// console.log("🚀 ~ file: App.js:2 ~ root:", root)
 
 
-const header = React.createElement("div",{id:'parent'},
-    React.createElement('div',{id:'children'},
-    [React.createElement('div',{},'this is a child1'),React.createElement('div',{},'this is a child2')]
-    ));
+const header = React.createElement("header",{id:'parent'},'Hello React');
 
-    console.log("🚀 ~ file: App.js:4 ~ header:", header)
+    // console.log("🚀 ~ file: App.js:4 ~ header:", header)
         
 root.render(header);
